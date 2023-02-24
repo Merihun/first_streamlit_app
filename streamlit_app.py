43dbfa73a2e1cf33e6c94dedeec5fbd358bf4da6
Merihun Dingeto <26862785+Merihun@users.noreply.github.com>
2023-02-24 16:29:45 -0500

    Update streamlit_app.py
    don't run while we troubleshoot

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-
+# don't run past here while we troubleshoot
+stremlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")

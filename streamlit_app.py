2bb52262298a48878a4509f589648518063fb1bd
Merihun Dingeto <26862785+Merihun@users.noreply.github.com>
2023-02-24 15:45:02 -0500

    Update streamlit_app.py
    miner code fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-import pandas as pd
+import pandas
 import requests
 import snowflake.connector
 
@@ -14,7 +14,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -33,6 +33,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_ch
 # streamlit.text(fruityvice_response.json()) --remove this line
 
 # take the json version of the response and normalize it 
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)

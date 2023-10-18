ca07f42ca47307c51b3c09f37037b32e4aaaee4e
Merihun Dingeto <26862785+Merihun@users.noreply.github.com>
2023-10-18 12:03:10 -0400

    Query Trial Account Metada

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,13 @@ streamlit.dataframe(fruits_to_show)
 # create the repeatable code block ( called a function)
 
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get(
         "https://fruityvice.com/api/fruit/" + this_fruit_choice)

9471c2bf54771d0badab9f389feac746cf7819c0
Merihun Dingeto <26862785+Merihun@users.noreply.github.com>
2023-10-30 16:38:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,14 +29,13 @@ streamlit.dataframe(fruits_to_show)
 
 # create the repeatable code block ( called a function)
 
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("SELECT * from fruit_load_list")
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
+streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
 
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get(
